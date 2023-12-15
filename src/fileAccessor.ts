1b4192fa13038ecdee07c7450e647946d2a496ee
Neil Campbell <neil.campbell@makerx.com.au>
2023-12-15 09:33:50 +0800

    fix: update to latest debugger

--- src/fileAccessor.ts
+++ src/fileAccessor.ts
@@ -4,15 +4,15 @@ import * as vscode from 'vscode'
 export const workspaceFileAccessor: FileAccessor = {
   isWindows: typeof process !== 'undefined' && process.platform === 'win32',
   readFile(path: string): Promise<Uint8Array> {
-    const uri = pathToUri(path)
+    const uri = vscode.Uri.file(path)
     return thenableToPromise(vscode.workspace.fs.readFile(uri))
   },
   writeFile(path: string, contents: Uint8Array): Promise<void> {
-    const uri = pathToUri(path)
+    const uri = vscode.Uri.file(path)
     return thenableToPromise(vscode.workspace.fs.writeFile(uri, contents))
   },
   basename(path: string): string {
-    const uri = pathToUri(path)
+    const uri = vscode.Uri.file(path)
     const lastSlash = uri.path.lastIndexOf('/')
     if (lastSlash === -1) {
       return path
@@ -43,14 +43,6 @@ export const workspaceFileAccessor: FileAccessor = {
 
     return fullUri.fsPath
   },
-}
-
-function pathToUri(path: string) {
-  try {
-    return vscode.Uri.file(path)
-  } catch (e) {
-    return vscode.Uri.parse(path, true)
-  }
 }
 
 function thenableToPromise<T>(t: Thenable<T>): Promise<T> {
